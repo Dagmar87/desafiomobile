f69e7686eea1cf74c2c229ddc8e830347146cfdc
Dagmar87 <jdfssobrinho@hotmail.com>
2021-07-29 20:43:25 -0300

    adicionar db

---
+++ src/main/resources/desafiomobileSQL.sql
@@ -0,0 +1,9 @@
+Use desafiomobile;
+
+CREATE TABLE nota(
+	id int auto_increment not null primary key,
+    titulo varchar(255) not null,
+    texto varchar(1000) not null
+);
+
+select * from desafiomobile.nota;
